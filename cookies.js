4e615602e372980bc413a91bfa3a6f57346ab4d5
Natalia <156667582+natinat95@users.noreply.github.com>
2024-11-23 21:09:15 +0100

    Add files via upload

--- cookies.js
+++ cookies.js
@@ -5,14 +5,14 @@ document.addEventListener('DOMContentLoaded', function () {
     // Verifica si ya se aceptaron las cookies
     if (!localStorage.getItem('cookiesAccepted')) {
         // Si no se han aceptado, muestra el banner
-        cookieBanner.style.bottom = '0';
+        cookieBanner.classList.add('show');
     }
 
     // Al hacer clic en el botón de aceptar cookies
     acceptCookiesButton.addEventListener('click', function () {
         console.log("Botón de aceptar cookies clickeado"); // Verifica si el evento se ejecuta
-        // Oculta el banner de cookies de inmediato cambiando el estilo de bottom
-        cookieBanner.style.bottom = '-150px';
+        // Oculta el banner de cookies de inmediato cambiando el estilo de opacidad
+        cookieBanner.classList.remove('show');
         // Guarda en el almacenamiento local que se aceptaron las cookies
         localStorage.setItem('cookiesAccepted', 'true');
     });
