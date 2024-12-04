50aefb8b2b70765321e511bf3ad777fcb7f3b86f
Natalia <156667582+natinat95@users.noreply.github.com>
2024-12-04 13:08:34 +0100

    Add files via upload

--- cookies.js
+++ cookies.js
@@ -1,19 +1,15 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const cookieBanner = document.getElementById('cookieBanner');
-    const acceptCookiesButton = document.getElementById('acceptCookies');
 
-    // Verifica si ya se aceptaron las cookies
+document.addEventListener('DOMContentLoaded', function() {
+    const cookieDialog = document.getElementById('cookieDialog');
+    const acceptButton = document.getElementById('acceptCookies');
+  
+    // Comprueba si el aviso ya ha sido aceptado
     if (!localStorage.getItem('cookiesAccepted')) {
-        // Si no se han aceptado, muestra el banner
-        cookieBanner.classList.add('show');
+      cookieDialog.showModal();
     }
-
-    // Al hacer clic en el botón de aceptar cookies
-    acceptCookiesButton.addEventListener('click', function () {
-        console.log("Botón de aceptar cookies clickeado"); // Verifica si el evento se ejecuta
-        // Oculta el banner de cookies de inmediato cambiando el estilo de opacidad
-        cookieBanner.classList.remove('show');
-        // Guarda en el almacenamiento local que se aceptaron las cookies
-        localStorage.setItem('cookiesAccepted', 'true');
+  
+    acceptButton.addEventListener('click', function() {
+      localStorage.setItem('cookiesAccepted', 'true');
+      cookieDialog.close();
     });
-});
+  });
